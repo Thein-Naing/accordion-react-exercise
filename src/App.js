e21e1b3f54281cfb261b47333deb7a8e0a040232
Thein Naing <theinnaing0908@gmail.com>
2023-05-11 13:22:54 +0800

    map the questions and return <SingleQuestion /> component with (question.key}{... question}

--- src/App.js
+++ src/App.js
@@ -5,7 +5,22 @@ import SingleQuestion from './Question';
 function App() {
   const[questions, setQuestions]   = useState(data);
 
-  return <h2>accordion project setup</h2>;
+
+  //  map the questions and return <SingleQuestion /> component with (question.key}{... question}
+  return (
+    <main>
+      <div className="container">
+        <h3> questions and answers about login</h3>
+        <section className='info'>
+          { questions.map((question) => {
+            return(
+            <SingleQuestion key={question.id} {...question}/>
+            )
+          })};
+        </section>
+        </div>
+    </main>
+  )
 }
 
 export default App;
